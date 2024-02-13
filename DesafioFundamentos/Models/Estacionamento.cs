a59e9598be7d540916a006e0e84efb3e5915b855
Bruna Soares <brunasoaresconce@gmail.com>
2024-02-13 16:30:16 -0300

    d

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -69,6 +69,7 @@ namespace DesafioFundamentos.Models
             else
             {
                 Console.WriteLine("Não há veículos estacionados.");
+                char
             }
         }
     }
